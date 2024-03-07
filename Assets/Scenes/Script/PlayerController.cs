3e2cf0930e4881b16d32f6c4497fb824fbd5c1d3
Jayabharathi3 <bharathibhuvana10@gmail.com>
2024-03-07 20:36:21 +0530

    updated aim, result and partial algorithm

--- Assets/Scenes/Script/PlayerController.cs
+++ Assets/Scenes/Script/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float xForce = 5.0f;
+    public float zForce = 5.0f;
+    public float yForce = 200.0f;
    
     // Start is called before the first frame update
     void Start()
@@ -14,6 +17,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // type the code here
-    }
+        float x = 0.0f
+        if(Input.GetKey(KeyCode.A))
+        {
+            x = x - xForce;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            x = x + xForce;
+        }
+        float z = 0.0f
+        if (Input.GetKey(KeyCode.S))
+        {
+            z = z - zForce;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            z = z + zForce;
+        }
+    }
 }
+
